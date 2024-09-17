99c60d2bdda577390d90d8e134430e061fdfd2f3
ahsunalla.patan@gmail.com <ahsunalla.patan@gmail.com>
2024-09-17 12:16:50 +0200

    Test
    Some Changes

--- test.sh
+++ test.sh
@@ -5,6 +5,17 @@
 in="abbabaq"
 out="0,3,5;"
 
-[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "Test 1 PASSED" || echo "Test 2 FAILED"
 
 
+in="aacq"
+out="0;"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "Test 2 PASSED" || echo "Test 2 FAILED"
+
+
+in="ababaaq"
+out="0,2,4,5;"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "Test 3 PASSED" || echo "Test 3 FAILED"
+
+
+# Use this code in the terminal to run the tests: ./test.sh
